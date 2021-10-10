67a4ef7f2479534dc32bcc47a14f840951bd7fcc
ub1n <bin951024@naver.com>
2021-10-10 19:40:57 +0900

    1주차 필수과제

---
+++ app/src/main/java/kr/ac/smu/cs/soptandroid/SignUpActivity.kt
@@ -0,0 +1,24 @@
+package kr.ac.smu.cs.soptandroid
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kr.ac.smu.cs.soptandroid.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        binding.buttonSiginIn.setOnClickListener {
+            if (binding.upEditId.text == null || binding.upEditName.text == null || binding.upEditPwd == null) {
+                Toast.makeText(this, "입력하지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+            } else {
+                finish()
+            }
+        }
+        setContentView(binding.root)
+    }
+}
